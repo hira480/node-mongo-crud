c4b861b5075aaf61b9aca646a5b0717e91dd1558
hira480 <hiramannan8@gmail.com>
2022-04-24 00:35:38 +0600

    update user added

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
         res.send(users);
     })
 
+    app.get('/user/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await userCollection.findOne(query);
+        res.send(result);
+    })
+
     // POST user: add a new user
     app.post('/user', async (req, res) => {
         const newUser = req.body;
@@ -36,11 +43,28 @@ async function run() {
         res.send(result);
     })
 
+    // update user
+    app.put('/user/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatedUser = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: {
+                name: updatedUser.name,
+                email: updatedUser.email
+            }
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+    })
+
     // delete a user
     app.delete('/user/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
-
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
     })
 }
 
